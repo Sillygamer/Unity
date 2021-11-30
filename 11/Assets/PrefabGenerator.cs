5e5b233473b50a722432c2778929c063e6f80a99
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2021-11-30 12:08:33 -0800

    11 Done

---
+++ 11/Assets/PrefabGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
